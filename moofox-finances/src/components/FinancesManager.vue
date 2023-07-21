6963819eb7c5837ed7257bb81f86b6adc5aafbb5
GroovyWizard <lucasfelipe260702@gmail.com>
2023-07-20 23:42:55 -0300

    Removed formCount variable, now the source of truth for the amount of forms in FinancesManager is the formList itself, so no more redundancy | Added button to remove finance form (frontend)

--- moofox-finances/src/components/FinancesManager.vue
+++ moofox-finances/src/components/FinancesManager.vue
@@ -1,8 +1,12 @@
 <template>
     <h3> New Item</h3>
-    <div v-for="count in formCount">
-        {{ count }}
-        <FinanceForm :financeForm="financeList[0]" />
+    <div v-for="(item, index) in financeList" :key="index">
+        {{ index }}
+        <v-btn @click="removeSelectedForm(item.id)" icon="mdi-delete" color="danger">
+        </v-btn>
+        <FinanceForm :financeForm="item" />
+
+
     </div>
     <v-row class="mt-2">
         <v-col cols="12">
@@ -40,22 +44,24 @@ export default {
                     dueDate: "",
                 }
             ],
-            formCount: 1,
-           
         };
     },
     methods: {
+        removeSelectedForm(formId) {
+            const itemToRemove = this.financeList.findIndex(form => form.id === formId)
+            if (itemToRemove !== -1) {
+                this.financeList.splice(itemToRemove, 1);
+            }
+        },
         addNewForm() {
             const defaultFormStructure = {
-                    id: crypto.randomUUID(),
-                    name: "",
-                    value: 0,
-                    payTo: "",
-                    dueDate: "",
+                id: crypto.randomUUID(),
+                name: "",
+                value: 0,
+                payTo: "",
+                dueDate: "",
             };
             this.financeList.push(defaultFormStructure);
-            this.formCount++;
-            console.log(this.financeList)
         }
     }
 
